dcb45c6e4e9771ff810b320d27d69787b4f77000
AmmarPendroy <ammar.muhammed@geg-construction.com>
2025-04-15 13:34:02 +0300

    Update emailer.py

--- utils/emailer.py
+++ utils/emailer.py
@@ -1,1 +1,18 @@
+import smtplib
+from email.mime.text import MIMEText
+import streamlit as st
 
+def send_email(subject, message, to_email):
+    try:
+        msg = MIMEText(message, "plain")
+        msg["Subject"] = subject
+        msg["From"] = st.secrets["email"]["sender"]
+        msg["To"] = to_email
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+            server.login(st.secrets["email"]["sender"], st.secrets["email"]["password"])
+            server.send_message(msg)
+        return True
+    except Exception as e:
+        st.error(f"Email failed: {e}")
+        return False
