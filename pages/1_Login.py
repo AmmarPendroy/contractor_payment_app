0f53ab63285fe3cde4647764b65709c3e70f852b
AmmarPendroy <ammar.muhammed@geg-construction.com>
2025-04-16 09:10:11 +0300

    Update 1_Login.py

--- pages/1_Login.py
+++ pages/1_Login.py
@@ -1,8 +1,9 @@
 import streamlit as st
+
+st.set_page_config(page_title="Login", layout="centered")
+
 import sys
 import os
-
-# Ensure utils folder is in path (for Streamlit Cloud)
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from utils.auth import login_user, register_user
@@ -15,8 +16,6 @@ if "user" in st.session_state:
         st.markdown('<style>.sidebar .sidebar-content { background-color: #E6F0FF; }</style>', unsafe_allow_html=True)
     elif role == "contractor":
         st.markdown('<style>.sidebar .sidebar-content { background-color: #E8F8F0; }</style>', unsafe_allow_html=True)
-
-st.set_page_config(page_title="Login", layout="centered")
 
 st.title("🔐 Login to Your Account")
 
