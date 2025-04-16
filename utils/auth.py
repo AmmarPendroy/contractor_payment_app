79c67632c92e1f28e63e6662af9e3928ec612464
AmmarPendroy <ammar.muhammed@geg-construction.com>
2025-04-16 10:11:06 +0300

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from firebase_config import auth, db
+
 import datetime
 
 SUPER_ADMIN_EMAIL = "ammar.muhammed@geg-construction.com"
